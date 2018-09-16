3ddb8e755d4b437cf6b037fcccea5ffc4e3bfec3
kenner1097 <kenner1097@hotmail.com>
2018-09-15 20:09:53 -0500

    Actualizo controller y registro

--- src/main/java/com/devweb/dbaccess/controller/AccessController.java
+++ src/main/java/com/devweb/dbaccess/controller/AccessController.java
@@ -1,15 +1,25 @@
 package com.devweb.dbaccess.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.devweb.dbaccess.entity.register;
+
 
 @Controller
 @RequestMapping("/access")
 public class AccessController {
 	
 	@GetMapping("/register")
-	public String register () {
+	public String Newregister (Model model) {
+		List<register> registro = new ArrayList<>();
+		
 		return "register_html";
 	}
+
 }
